11907d9beedb861a87b3d3e968e866072c58ee53
shivpr116 <52942124+shivpr116@users.noreply.github.com>
2019-07-29 20:51:20 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,10 @@
+import {songs} from '../../app/songInfo';
+
+export const mysongs: songs[] = [
+{name: 'Ishq mitha', artist: 'ankit', genre:'classic', released:'2000', picture:'daler.jpg'},
+{name: 'High Life', artist: 'sid', genre:'unknown', released:'2050', picture:'sultan.jpg'},
+{name: 'Breakup song', artist: 'boli', genre:'pop', released:'1990', picture:'arijit.jpg'},
+{name: 'Dil le gyi', artist: 'kitanu', genre:'disturbing', released:'1850', picture:'narendramodi.jpg'},
+
+
+];
